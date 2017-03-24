897f17f565da91b88f579e94dcc3f12c645152b5
Francisco Neves <fntneves@gmail.com>
2017-03-24 20:08:01 +0000

    Add package structure

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.md").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="vania",
+    version="0.1.0",
+    description="A module to fairly distribute tasks considering people preferences.",
+    license="MIT",
+    author="Hackathonners",
+    packages=find_packages(),
+    install_requires=[],
+    package_dir={'': 'src'},
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
