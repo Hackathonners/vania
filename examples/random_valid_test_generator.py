ec884c9db173f093d1398de54d00f1c36f22d8e4
Francisco Neves <fntneves@gmail.com>
2017-03-25 17:31:16 +0000

    Reformat random generator reformat code

--- examples/random_valid_test_generator.py
+++ examples/random_valid_test_generator.py
@@ -1,13 +1,14 @@
 import sys
 import time
 from random import shuffle
-from FairDistributor import FairDistributor
+from vania.fair_distributor import FairDistributor
 
 
 def main():
     # User input for the number of targets and objects.
     number_of_targets = int(sys.argv[1])
     number_of_objects = int(sys.argv[2])
+
     # Generate dummy lists for objects, targets and dummy matrix for weights
     targets = ['target_{0}'.format(str(s))
                for s in range(number_of_targets)]
@@ -19,10 +20,14 @@ def main():
         new_random_weight_list = list(dummy_weights)
         shuffle(new_random_weight_list)
         weights_matrix.append(new_random_weight_list)
+
+    # Benchmark solver
     start_time = time.time()
     distributor = FairDistributor(targets, objects, weights_matrix)
     distributor.distribute()
     elapsed_time = time.time() - start_time
+
+    # Output
     print('Number of Targets: {0}\nNumber of Objects: {1}\nTime elapsed: {2}'.format(
         number_of_targets, number_of_objects, elapsed_time))
 
