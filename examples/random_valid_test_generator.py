868a38476d36573933a8481dbed2e69150fcbb61
cclauss <cclauss@bluewin.ch>
2017-06-20 10:43:05 +0200

    Str(i) is redundant inside format(i) (#9)

--- examples/random_valid_test_generator.py
+++ examples/random_valid_test_generator.py
@@ -7,10 +7,8 @@ def main():
     number_of_objects = int(sys.argv[2])
 
     # Generate dummy lists for objects, targets and dummy matrix for weights
-    targets = ['target_{0}'.format(str(s))
-               for s in range(number_of_targets)]
-    objects = ['object_{0}'.format(str(s))
-               for s in range(number_of_objects)]
+    targets = ['target_{}'.format(s) for s in range(number_of_targets)]
+    objects = ['object_{}'.format(s) for s in range(number_of_objects)]
     dummy_weights = list(range(1, number_of_objects+1))
     weights_matrix = list()
     for _ in range(number_of_targets):
